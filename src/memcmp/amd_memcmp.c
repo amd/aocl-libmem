1cbdd4243cc47f35b6a255b96f3596a71d294d6e
Sajan Karumanchi <sajan.karumanchi@amd.com>
2022-08-30 01:06:22 +0530

    AOCL-LibMem: Adding AVX512 support for memcmp
    Adding AVX512 instruction support for memcmp to the library.
    
    Change-Id: Ib0eeef3af1631c42e5d751b60b174b74601236d3

--- src/memcmp/amd_memcmp.c
+++ src/memcmp/amd_memcmp.c
@@ -27,12 +27,13 @@
 #include "logger.h"
 #include <immintrin.h>
 #include <stdint.h>
+#include "zen_cpu_info.h"
 
-static int memcmp_below_64(const void *mem1, const void *mem2, size_t size)
+static int memcmp_le_2ymm(const void *mem1, const void *mem2, size_t size)
 {
     __m256i y0, y1, y2, y3;
     __m128i x0, x1, x2, x3;
-    int32_t ret = 0;
+    uint32_t cmp_mask = 0;
 
     if (size == 0)
         return 0;
@@ -41,116 +42,201 @@ static int memcmp_below_64(const void *mem1, const void *mem2, size_t size)
     {
         return *((uint8_t *)mem1) != *((uint8_t *)mem2);
     }
-    if (size <= 4)
+    if (size <= 2 * WORD_SZ)
     {
         return ((*((uint16_t *)mem1) != *((uint16_t *)mem2)) | 
-        (*((uint16_t *)(mem1 + size - 2)) != *((uint16_t *)(mem2 + size -2))));
+        (*((uint16_t *)(mem1 + size - WORD_SZ)) != \
+                        *((uint16_t *)(mem2 + size -WORD_SZ))));
     }
-    if (size <= 8)
+    if (size <= 2 * DWORD_SZ)
     {
         return ((*((uint32_t *)mem1) != *((uint32_t *)mem2)) | 
-        (*((uint32_t *)(mem1 + size - 4)) != *((uint32_t *)(mem2 + size - 4))));
+        (*((uint32_t *)(mem1 + size - DWORD_SZ)) != \
+                    *((uint32_t *)(mem2 + size - DWORD_SZ))));
     }
-    if (size <= 16)
+    if (size <= 2 * QWORD_SZ)
     {
         return ((*((uint64_t*)mem1)) != (*((uint64_t*)mem2))) | 
-        ((*((uint64_t *)(mem1 + size - 8))) != (*((uint64_t*)(mem2 + size - 8))));
+        ((*((uint64_t *)(mem1 + size - QWORD_SZ))) != \
+                    (*((uint64_t*)(mem2 + size - QWORD_SZ))));
     }
-    if (size <= 32)
+    if (size <= 2 * XMM_SZ)
     {
         x0 = _mm_loadu_si128(mem1);
         x1 = _mm_loadu_si128(mem2);
         x0 = _mm_cmpeq_epi8(x0, x1);
-        ret = _mm_movemask_epi8(x0);
-        x2 = _mm_loadu_si128(mem1 + size - 16);
-        x3 = _mm_loadu_si128(mem2 + size - 16);
+        cmp_mask = _mm_movemask_epi8(x0);
+        x2 = _mm_loadu_si128(mem1 + size - XMM_SZ);
+        x3 = _mm_loadu_si128(mem2 + size - XMM_SZ);
         x2 = _mm_cmpeq_epi8(x2, x3);
-        ret &= _mm_movemask_epi8(x2);
-        return (uint16_t)ret ^ 0xffff; 
+        cmp_mask &= _mm_movemask_epi8(x2);
+        return (cmp_mask != (uint16_t)-1);
     }
-    else
-    {
-        y0 = _mm256_loadu_si256(mem1);
-        y1 = _mm256_loadu_si256(mem2);
-        y0 = _mm256_cmpeq_epi8(y0, y1);
-        ret =  _mm256_movemask_epi8(y0);
-        y2 = _mm256_loadu_si256(mem1 + size - 32);
-        y3 = _mm256_loadu_si256(mem2 + size - 32);
-        y2 = _mm256_cmpeq_epi8(y2, y3);
-        ret &=  _mm256_movemask_epi8(y2);
-        return (uint32_t)ret ^ 0xffffffff; 
-    }
-    return -1;
+    y0 = _mm256_loadu_si256(mem1);
+    y1 = _mm256_loadu_si256(mem2);
+    y0 = _mm256_cmpeq_epi8(y0, y1);
+    cmp_mask =  _mm256_movemask_epi8(y0);
+    y2 = _mm256_loadu_si256(mem1 + size - YMM_SZ);
+    y3 = _mm256_loadu_si256(mem2 + size - YMM_SZ);
+    y2 = _mm256_cmpeq_epi8(y2, y3);
+    cmp_mask &=  _mm256_movemask_epi8(y2);
+    return (cmp_mask != (uint32_t)-1);
 }
+
 static int unaligned_ld_cmp(const void *mem1, const void *mem2, size_t size)
 {
     __m256i y0, y1, y2, y3, y4, y5, y6, y7;
     size_t offset = 0;
-    int32_t ret = 0;
-    if (size <= 128)
+    uint32_t cmp_mask = 0;
+
+    if (size <= 4 * YMM_SZ)
     {
-        y0 = _mm256_loadu_si256(mem2 + 0 * 32);
-        y1 = _mm256_loadu_si256(mem2 + 1 * 32);
-        y4 = _mm256_loadu_si256(mem1 + 0 * 32);
-        y5 = _mm256_loadu_si256(mem1 + 1 * 32);
-        y2 = _mm256_loadu_si256(mem2 + size - 1 * 32);
-        y3 = _mm256_loadu_si256(mem2 + size - 2 * 32);
-        y6 = _mm256_loadu_si256(mem1 + size - 1 * 32);
-        y7 = _mm256_loadu_si256(mem1 + size - 2 * 32);
+        y0 = _mm256_loadu_si256(mem2 + 0 * YMM_SZ);
+        y1 = _mm256_loadu_si256(mem2 + 1 * YMM_SZ);
+        y4 = _mm256_loadu_si256(mem1 + 0 * YMM_SZ);
+        y5 = _mm256_loadu_si256(mem1 + 1 * YMM_SZ);
+        y2 = _mm256_loadu_si256(mem2 + size - 1 * YMM_SZ);
+        y3 = _mm256_loadu_si256(mem2 + size - 2 * YMM_SZ);
+        y6 = _mm256_loadu_si256(mem1 + size - 1 * YMM_SZ);
+        y7 = _mm256_loadu_si256(mem1 + size - 2 * YMM_SZ);
         y0 = _mm256_cmpeq_epi8(y0, y4);
         y1 = _mm256_cmpeq_epi8(y1, y5);
         y2 = _mm256_cmpeq_epi8(y2, y6);
         y3 = _mm256_cmpeq_epi8(y3, y7);
-        ret = _mm256_movemask_epi8(y0) & _mm256_movemask_epi8(y1);
-        ret &= _mm256_movemask_epi8(y2) & _mm256_movemask_epi8(y3);
-        return ret ^ 0xffffffff;
+        cmp_mask = _mm256_movemask_epi8(y0) & _mm256_movemask_epi8(y1);
+        cmp_mask &= _mm256_movemask_epi8(y2) & _mm256_movemask_epi8(y3);
+        return (cmp_mask != (uint32_t)-1);
     }
     while (size > offset)
     {
-        y0 = _mm256_loadu_si256(mem2 + offset + 0 * 32);
-        y1 = _mm256_loadu_si256(mem2 + offset + 1 * 32);
-        y2 = _mm256_loadu_si256(mem2 + offset + 2 * 32);
-        y3 = _mm256_loadu_si256(mem2 + offset + 3 * 32);
-        y4 = _mm256_loadu_si256(mem1 + offset + 0 * 32);
-        y5 = _mm256_loadu_si256(mem1 + offset + 1 * 32);
-        y6 = _mm256_loadu_si256(mem1 + offset + 2 * 32);
-        y7 = _mm256_loadu_si256(mem1 + offset + 3 * 32);
+        y0 = _mm256_loadu_si256(mem2 + offset + 0 * YMM_SZ);
+        y1 = _mm256_loadu_si256(mem2 + offset + 1 * YMM_SZ);
+        y2 = _mm256_loadu_si256(mem2 + offset + 2 * YMM_SZ);
+        y3 = _mm256_loadu_si256(mem2 + offset + 3 * YMM_SZ);
+        y4 = _mm256_loadu_si256(mem1 + offset + 0 * YMM_SZ);
+        y5 = _mm256_loadu_si256(mem1 + offset + 1 * YMM_SZ);
+        y6 = _mm256_loadu_si256(mem1 + offset + 2 * YMM_SZ);
+        y7 = _mm256_loadu_si256(mem1 + offset + 3 * YMM_SZ);
         y0 = _mm256_cmpeq_epi8(y0, y4);
         y1 = _mm256_cmpeq_epi8(y1, y5);
         y2 = _mm256_cmpeq_epi8(y2, y6);
         y3 = _mm256_cmpeq_epi8(y3, y7);
-        ret = _mm256_movemask_epi8(y0) & _mm256_movemask_epi8(y1);
-        ret &= _mm256_movemask_epi8(y2) & _mm256_movemask_epi8(y3);
-        if (ret ^ 0xffffffff)
+        cmp_mask = _mm256_movemask_epi8(y0) & _mm256_movemask_epi8(y1);
+        cmp_mask &= _mm256_movemask_epi8(y2) & _mm256_movemask_epi8(y3);
+        if (cmp_mask != (uint32_t)-1)
             return -1;
-        y0 = _mm256_loadu_si256(mem2 + size - 1 * 32);
-        y1 = _mm256_loadu_si256(mem2 + size - 2 * 32);
-        y2 = _mm256_loadu_si256(mem2 + size - 3 * 32);
-        y3 = _mm256_loadu_si256(mem2 + size - 4 * 32);
-        y4 = _mm256_loadu_si256(mem1 + size - 1 * 32);
-        y5 = _mm256_loadu_si256(mem1 + size - 2 * 32);
-        y6 = _mm256_loadu_si256(mem1 + size - 3 * 32);
-        y7 = _mm256_loadu_si256(mem1 + size - 4 * 32);
+        y0 = _mm256_loadu_si256(mem2 + size - 1 * YMM_SZ);
+        y1 = _mm256_loadu_si256(mem2 + size - 2 * YMM_SZ);
+        y2 = _mm256_loadu_si256(mem2 + size - 3 * YMM_SZ);
+        y3 = _mm256_loadu_si256(mem2 + size - 4 * YMM_SZ);
+        y4 = _mm256_loadu_si256(mem1 + size - 1 * YMM_SZ);
+        y5 = _mm256_loadu_si256(mem1 + size - 2 * YMM_SZ);
+        y6 = _mm256_loadu_si256(mem1 + size - 3 * YMM_SZ);
+        y7 = _mm256_loadu_si256(mem1 + size - 4 * YMM_SZ);
         y0 = _mm256_cmpeq_epi8(y0, y4);
         y1 = _mm256_cmpeq_epi8(y1, y5);
         y2 = _mm256_cmpeq_epi8(y2, y6);
         y3 = _mm256_cmpeq_epi8(y3, y7);
-        ret = _mm256_movemask_epi8(y0) & _mm256_movemask_epi8(y1);
-        ret &= _mm256_movemask_epi8(y2) & _mm256_movemask_epi8(y3);
-        if (ret ^ 0xffffffff)
+        cmp_mask = _mm256_movemask_epi8(y0) & _mm256_movemask_epi8(y1);
+        cmp_mask &= _mm256_movemask_epi8(y2) & _mm256_movemask_epi8(y3);
+        if (cmp_mask != (uint32_t)-1)
             return -1;
-        offset +=128;
-        size -= 128;
+        offset += 4 * YMM_SZ;
+        size -= 4 * YMM_SZ;
     }
-    return (ret ^ 0xffffffff);
+    return (cmp_mask != (uint32_t)-1);
 }
+
+#ifdef AVX512_FEATURE_ENABLED
+static int unaligned_avx512_ld_cmp(const void *mem1, const void *mem2, size_t size)
+{
+    __m512i z0, z1, z2, z3, z4, z5, z6, z7;
+    size_t offset = 0;
+    __mmask64 mask1, mask2, mask3, mask4;
+
+    if (size <= 2 * ZMM_SZ)
+    {
+        z0 = _mm512_loadu_si512(mem2 + 0 * ZMM_SZ);
+        z1 = _mm512_loadu_si512(mem2 + size - 1 * ZMM_SZ);
+        z2 = _mm512_loadu_si512(mem1 + 0 * ZMM_SZ);
+        z3 = _mm512_loadu_si512(mem1 + size - 1 * ZMM_SZ);
+
+        mask1 = _mm512_cmpeq_epu8_mask(z0, z2);
+        mask2 = _mm512_cmpeq_epu8_mask(z1, z3);
+        mask1 &= mask2;
+        return (mask1 != (uint64_t)-1);
+    }
+
+    if (size <= 4 * ZMM_SZ)
+    {
+        z0 = _mm512_loadu_si512(mem2 + 0 * ZMM_SZ);
+        z1 = _mm512_loadu_si512(mem2 + 1 * ZMM_SZ);
+        z4 = _mm512_loadu_si512(mem1 + 0 * ZMM_SZ);
+        z5 = _mm512_loadu_si512(mem1 + 1 * ZMM_SZ);
+        z2 = _mm512_loadu_si512(mem2 + size - 1 * ZMM_SZ);
+        z3 = _mm512_loadu_si512(mem2 + size - 2 * ZMM_SZ);
+        z6 = _mm512_loadu_si512(mem1 + size - 1 * ZMM_SZ);
+        z7 = _mm512_loadu_si512(mem1 + size - 2 * ZMM_SZ);
+        mask1 = _mm512_cmpeq_epu8_mask(z0, z4);
+        mask2 = _mm512_cmpeq_epu8_mask(z1, z5);
+        mask3 = _mm512_cmpeq_epu8_mask(z2, z6);
+        mask4 = _mm512_cmpeq_epu8_mask(z3, z7);
+        mask1 &= mask2 & mask3 & mask4;
+        return (mask1 != (uint64_t)-1);
+    }
+    while (size > offset)
+    {
+        z0 = _mm512_loadu_si512(mem2 + offset + 0 * ZMM_SZ);
+        z1 = _mm512_loadu_si512(mem2 + offset + 1 * ZMM_SZ);
+        z2 = _mm512_loadu_si512(mem2 + offset + 2 * ZMM_SZ);
+        z3 = _mm512_loadu_si512(mem2 + offset + 3 * ZMM_SZ);
+        z4 = _mm512_loadu_si512(mem1 + offset + 0 * ZMM_SZ);
+        z5 = _mm512_loadu_si512(mem1 + offset + 1 * ZMM_SZ);
+        z6 = _mm512_loadu_si512(mem1 + offset + 2 * ZMM_SZ);
+        z7 = _mm512_loadu_si512(mem1 + offset + 3 * ZMM_SZ);
+        mask1 = _mm512_cmpeq_epu8_mask(z0, z4);
+        mask2 = _mm512_cmpeq_epu8_mask(z1, z5);
+        mask3 = _mm512_cmpeq_epu8_mask(z2, z6);
+        mask4 = _mm512_cmpeq_epu8_mask(z3, z7);
+
+        mask1 &= mask2 & mask3 & mask4;
+
+        if (mask1 != (uint64_t)-1)
+            return -1;
+        z0 = _mm512_loadu_si512(mem2 + size - 1 * ZMM_SZ);
+        z1 = _mm512_loadu_si512(mem2 + size - 2 * ZMM_SZ);
+        z2 = _mm512_loadu_si512(mem2 + size - 3 * ZMM_SZ);
+        z3 = _mm512_loadu_si512(mem2 + size - 4 * ZMM_SZ);
+        z4 = _mm512_loadu_si512(mem1 + size - 1 * ZMM_SZ);
+        z5 = _mm512_loadu_si512(mem1 + size - 2 * ZMM_SZ);
+        z6 = _mm512_loadu_si512(mem1 + size - 3 * ZMM_SZ);
+        z7 = _mm512_loadu_si512(mem1 + size - 4 * ZMM_SZ);
+        mask1 = _mm512_cmpeq_epu8_mask(z0, z4);
+        mask2 = _mm512_cmpeq_epu8_mask(z1, z5);
+        mask3 = _mm512_cmpeq_epu8_mask(z2, z6);
+        mask4 = _mm512_cmpeq_epu8_mask(z3, z7);
+
+        mask1 &= mask2 & mask3 & mask4;
+
+        if (mask1 != (uint64_t)-1)
+            return -1;
+        offset += 4 * ZMM_SZ;
+        size -= 4 * ZMM_SZ;
+    }
+    return (mask1 != (uint64_t)-1);
+}
+#endif
 
 int amd_memcmp(const void *mem1, const void *mem2, size_t size)
 {
     LOG_INFO("\n");
-    if (size <= 64)
-        return memcmp_below_64(mem1, mem2, size);
+    if (size <= 2 * YMM_SZ)
+        return memcmp_le_2ymm(mem1, mem2, size);
+#ifdef AVX512_FEATURE_ENABLED
+   return unaligned_avx512_ld_cmp(mem1, mem2, size);
+#else
    return unaligned_ld_cmp(mem1, mem2, size);
+#endif
 }
 
 int memcmp(const void *, const void *, size_t) __attribute__((weak, alias("amd_memcmp"), visibility("default")));
