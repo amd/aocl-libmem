35365f88d860f25e4d8f6de1c1264a49d84c19ab
Sajan Karumanchi <sajan.karumanchi@amd.com>
2023-10-17 12:34:46 +0000

    AOCL-LibMem: memcpy optimization for avx512
    Added non-temporal threshold to optimize larger sizes
    for avx512 machines.
    Removed unlikely branching for size ZERO.
    
    AMD-Internal: [CPUPL-3862]
    Change-Id: Ib1c93691c271b0daaaaaf2d9e42818720e37d3f0

--- src/base_impls/load_store_avx512_impls.h
+++ src/base_impls/load_store_avx512_impls.h
@@ -53,13 +53,13 @@ static inline void *__load_store_ble_zmm_vec(void *store_addr, const void *load_
     __m512i z0, z1;
     __mmask64 mask;
 
-    if (size == 0)
-        return store_addr;
-
-    mask = ((uint64_t)-1) >> (64 - size);
-    z1 = _mm512_setzero_epi32();
-    z0 =  _mm512_mask_loadu_epi8(z1 ,mask, load_addr);
-    _mm512_mask_storeu_epi8(store_addr, mask, z0);
+    if (size)
+    {
+        mask = ((uint64_t)-1) >> (VEC_SZ_AVX512 - size);
+        z1 = _mm512_setzero_epi32();
+        z0 =  _mm512_mask_loadu_epi8(z1 ,mask, load_addr);
+        _mm512_mask_storeu_epi8(store_addr, mask, z0);
+    }
     return store_addr;
 }
 
